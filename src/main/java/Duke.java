243fdca8aec0645ecdf7cd79c50fcaa1fb902a8f
MuhammadHozefa <MuhammadHozefa@gmail.com>
2020-08-23 22:02:49 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,11 +7,31 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
 
+        System.out.println("Welcome to\n" + logo);
         System.out.println("--------------------------------------");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        System.out.println("--------------------------------------");
+
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+
+        while (!"bye".equals(command)) {
+            switch (command) {
+            case "list":
+                System.out.println("--------------------------------------");
+                System.out.println("list");
+                System.out.println("--------------------------------------");
+                break;
+            case "blah":
+                System.out.println("--------------------------------------");
+                System.out.println("blah");
+                System.out.println("--------------------------------------");
+                break;
+            }
+            command = sc.nextLine();
+        }
         System.out.println("--------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("--------------------------------------");
