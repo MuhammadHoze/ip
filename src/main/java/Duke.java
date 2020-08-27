b1e470a0db119c70803882c54b5e9164972c8cad
MuhammadHozefa <MuhammadHozefa@gmail.com>
2020-08-27 23:50:39 +0800

    Level3:Fixed Bugs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
     private static String[] instruction = new String[MAX_INSTRUCTION];
     private static Boolean[] instructionStatus = new Boolean[MAX_INSTRUCTION];
     private static int indexCount = 0; // Index to add in instructions to array
+    private static boolean isEmpty = true; // To check if Instruction array is empty
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -21,11 +22,20 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
 
+        // if "Enter key" is pressed WITHOUT any input
+        while(command.isEmpty()){
+            System.out.println("Invalid Input, Please Try Again");
+            command = sc.nextLine();
+        }
+
         while (!"bye".equals(command)) {
-            if (command.equalsIgnoreCase("list")) {
+            if (command.trim().equalsIgnoreCase("list")) {
                 System.out.println("----------------------------------------");
                 for (int i = 0; i < instruction.length; i++) {
                     if (instruction[i] == null) { // to prevent empty array values to show
+                        if (isEmpty) {
+                            System.out.println("List is currently empty");
+                        }
                         break;
                     }
                     else {
@@ -34,23 +44,38 @@ public class Duke {
                 }
                 System.out.println("----------------------------------------");
             }
-            else if (command.equalsIgnoreCase("bye")) {
+            else if (command.trim().equalsIgnoreCase("bye")) {
                 break;
             }
             else if (command.contains("done")) {
-
-                String value = command.substring(5);
-                int instructNum = Integer.parseInt(value);
-                setInstruction(instructNum);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[" + getStatusIcon(instructNum - 1) + "]" + " " + instruction[instructNum - 1]);
+                if (isEmpty){
+                    System.out.println("Please add instruction first!");
+                }
+                else {
+                    //This is to get the instruction number
+                    String value = command.replace("done", "").trim();
+                    int instructNum = Integer.parseInt(value);
+                    setInstruction(instructNum);
+                    System.out.println("----------------------------------------");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[" + getStatusIcon(instructNum - 1) + "]" + " " + instruction[instructNum - 1]);
+                    System.out.println("----------------------------------------");
+                }
             }
             else {
+                System.out.println("----------------------------------------");
+                System.out.println("added: "+ command );
+                System.out.println("----------------------------------------");
                 instruction[indexCount] = command;
                 instructionStatus[indexCount] = false; //Initially
+                isEmpty = false;
                 indexCount++;
             }
             command = sc.nextLine();
+            while(command.isEmpty()){
+                System.out.println("Invalid Input, Please Try Again");
+                command = sc.nextLine();
+            }
         }
         System.out.println("----------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
